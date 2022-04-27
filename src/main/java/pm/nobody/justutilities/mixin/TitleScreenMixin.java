ddcaa57e542ffac0852b142a2eedbe24af5a4dea
Nobody <nobodyawesomer@proton.me>
2022-04-27 16:36:35 -0700

    Customized template.
    Initial project setup with LICENSE.

--- src/main/java/pm/nobody/justutilities/mixin/TitleScreenMixin.java
+++ src/main/java/pm/nobody/justutilities/mixin/TitleScreenMixin.java
@@ -1,16 +1,17 @@
-package net.fabricmc.example.mixin;
+package pm.nobody.justutilities.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
+import pm.nobody.justutilities.JustUtilitiesMod;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class TitleScreenMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		JustUtilitiesMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
